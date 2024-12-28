73b93f63d4d6827b11d93190887c6895d381f6e7
Brahim Belmouhand <r0956428@ucll.be>
2024-12-28 04:34:34 +0100

    Back-end: Added conceptual game model

---
+++ back-end/model/game.ts
@@ -0,0 +1,36 @@
+class Game {
+    private id?: number;
+    private name: string;
+    private price: number;
+    private genres: string[];
+
+    constructor(game: { id?: number, name: string, price: number, genres: string[] }) {
+        this.id = game.id;
+        this.name = game.name;
+        this.price = game.price;
+        this.genres = game.genres;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+
+    getGenres(): string[] {
+        return this.genres;
+    }
+
+    validate(game: { name: string, price: number, genres: string[] }) {
+        if (!game.name) {
+            throw new Error("Name is required!");
+        }
+        if (!game.price) {
+            throw new Error("Price is required!");
+        }
+    }
+}
+
+export default Game;
