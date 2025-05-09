855862445790ec8fdad87a61b29dab0effbeaaad
Brahim Belmouhand <brahimbelmouhand@gmail.com>
2025-05-09 18:56:52 +0200

    Added iinput sanitization in the back-end

--- back-end/controller/admin.routes.ts
+++ back-end/controller/admin.routes.ts
@@ -1,10 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { Router } from "express";
 import adminService from '../service/admin.service';
-
+import sanitizehtml from "sanitize-html";
 const app = Router();
 app.post('/register', (req: Request, res: Response, next: NextFunction) => {
-    const username = req.body;
+    const username = sanitizehtml(req.body);
     const admin = adminService.createAdmin(username);
     res.status(200).json(admin);
 
--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -1,12 +1,20 @@
 import express, { NextFunction, Request, Response, Router } from 'express';
 import { UserInput } from '../types';
 import userService from '../service/user.service';
-
+import sanitizehtml from "sanitize-html";
 const userRouter = express.Router();
 
 userRouter.post('/register', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const newUserInput = <UserInput>req.body;
+        const sanitizedInput = {
+            firstName: sanitizehtml(req.body.firstName || ""),
+            lastName: sanitizehtml(req.body.lastName || ""),
+            email: sanitizehtml(req.body.email || ""),
+            username: sanitizehtml(req.body.username || ""),
+            password: req.body.password,
+            birthDate: new Date(req.body.birthDate),
+        };
+        const newUserInput = sanitizedInput as UserInput;
         const newUser = await userService.createUser(newUserInput);
         res.status(200).json(newUser);
     }
