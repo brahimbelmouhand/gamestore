bc252ae8254a49ec29a4759dbd6bd24ec60a3938
Brahim Belmouhand <brahimbelmouhand@gmail.com>
2025-05-10 23:52:26 +0200

    Added CSP for front-end

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,5 +1,17 @@
 const { i18n } = require('./next-i18next.config');
-
 module.exports = {
   i18n,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; object-src 'none'; frame-ancestors 'none';"
+          },
+        ],
+      },
+    ];
+  },
 };
