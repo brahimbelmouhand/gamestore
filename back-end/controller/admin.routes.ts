208312723f660ac315aae0d89d2c12b709fa7bdb
Brahim Belmouhand <brahimbelmouhand@gmail.com>
2025-05-11 09:03:59 +0200

    Installed more node packages

--- back-end/controller/admin.routes.ts
+++ back-end/controller/admin.routes.ts
@@ -1,16 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { Router } from "express";
 import adminService from '../service/admin.service';
 import sanitizehtml from "sanitize-html";
-const app = Router();
-app.post('/register', (req: Request, res: Response, next: NextFunction) => {
+const adminRouter = express.Router();
+
+adminRouter.post('/register', (req: Request, res: Response, next: NextFunction) => {
     const username = sanitizehtml(req.body);
     const admin = adminService.createAdmin(username);
     res.status(200).json(admin);
-
 });
-
-const adminRouter = express.Router();
-
 
 export default adminRouter;
